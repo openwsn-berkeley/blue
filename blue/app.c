4c9d8e9f3c212b54ea2ae6937c257de776db332a
Thomas Watteyne <twatteyne@gmail.com>
2022-03-20 20:51:19 +0100

    working system

--- blue/app.c
+++ blue/app.c
@@ -1,6 +1,14 @@
+#include <string.h>
 #include "nrf52840.h"
 
 //=========================== defines =========================================
+
+// simple program which sets LED2, crafted to be loaded at address 0x00080000
+const uint8_t newprogram[] = {
+    0x40,0xF2,0x0C,0x51, 0xC5,0xF2,0x00,0x01, 0x4F,0xF4,0x80,0x40, 0x08,0x60,0x40,0xBF,
+    0x20,0xBF,0x20,0xBF, 0xFB,0xE7,
+                                   0xff,0xff // padding
+}; 
 
 //=========================== prototypes ======================================
 
@@ -8,14 +16,18 @@
 
 //=========================== public ==========================================
 
-int __attribute((section(".ARM.__at_0x1000"))) app_main(void) {
+int __attribute((section(".app1"))) app_main(void) {
     
     // LED on
     NRF_P0->OUTCLR                     = (0x00000001 << 13);    // LED 1
-    //NRF_P0->OUTCLR                     = (0x00000001 << 14);    // LED 2
+    //NRF_P0->OUTCLR                   = (0x00000001 << 14);    // LED 2
+
+    // write new program in CONTAINER_APP_NEW
+    NRF_NVMC->CONFIG                   = 0x00000001; // 1 == Write enabled
+    memcpy((uint8_t*)(768*1024),&newprogram,sizeof(newprogram));
+    NRF_NVMC->CONFIG                   = 0x00000000; // 0 == Read only access
 
     // loop
-    /*
     while(1) {
 
         // wait for event
@@ -23,7 +35,6 @@ int __attribute((section(".ARM.__at_0x1000"))) app_main(void) {
         __WFE(); // wait for event
         __WFE(); // wait for event
     }
-    */
 }
 
 //=========================== private =========================================
--- blue/blue.c
+++ blue/blue.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "nrf52840.h"
 #include "app.h"
 
@@ -9,6 +10,10 @@
 // LED 3 P0.15
 // LED 4 P0.16
 
+#define CONTAINER_APP_CURRENT (512*1024)    // address of the container of the current app
+#define CONTAINER_APP_NEW     (768*1024)    // address of the container of the new app, if any
+#define CONTAINER_APP_LENGTH  (4*1024)      // length of an app container, in bytes
+
 //=========================== prototypes ======================================
 
 typedef int appfunc_t(void);
@@ -16,7 +21,8 @@ typedef int appfunc_t(void);
 //=========================== variables =======================================
 
 typedef struct {
-    uint32_t       dummy;
+    appfunc_t*     appfunc;
+    uint32_t*      word;
 } blue_vars_t;
 
 blue_vars_t blue_vars;
@@ -30,7 +36,11 @@ blue_dbg_t blue_dbg;
 //=========================== main ============================================
 
 int main(void) {
-    appfunc_t* appfunc = (appfunc_t*)0x00001001;
+    
+    // initialize
+    memset(&blue_vars,0x00,sizeof(blue_vars_t));
+
+    //=== set LEDs indicating execution started
 
     // all LEDs enabled
     NRF_P0->PIN_CNF[13]                = 0x00000003;            // LED 1
@@ -45,14 +55,45 @@ int main(void) {
     NRF_P0->OUTSET                     = (0x00000001 << 16);    // LED 4
 
     // LED3 on
-    //NRF_P0->OUTCLR                   = (0x00000001 << 13);    // LED 1
-    //NRF_P0->OUTCLR                   = (0x00000001 << 14);    // LED 2
     NRF_P0->OUTCLR                     = (0x00000001 << 15);    // LED 3
-    //NRF_P0->OUTCLR                   = (0x00000001 << 16);    // LED 4
     
-    // handover to app
-    app_main();
-    return appfunc();
+    //=== load new app, if needed
+
+    // WARNING: not immune to powering off during this section of code,
+    //     i.e. you brick the part if you switch off the device during step 2 or step 3
+
+    // determine whether there is anything in the container for new apps
+    blue_vars.word = (uint32_t*)(CONTAINER_APP_NEW);
+    if (*blue_vars.word!=0xffffffff) {
+        // there is a new app in the new app container@
+
+        // step 1. clear current app container
+        NRF_NVMC->CONFIG               = 0x00000002; // 2 == Erase enabled
+        NRF_NVMC->ERASEPAGE            = CONTAINER_APP_CURRENT;
+        while(NRF_NVMC->READY==0);
+        NRF_NVMC->CONFIG               = 0x00000000; // 0 == Read only access
+
+        // step 2. copy data over
+        NRF_NVMC->CONFIG               = 0x00000001; // 1 == Write enabled
+        memcpy((uint8_t*)(CONTAINER_APP_CURRENT),(uint8_t*)(CONTAINER_APP_NEW),CONTAINER_APP_LENGTH);
+        NRF_NVMC->CONFIG               = 0x00000000; // 0 == Read only access
+
+        // step 3. clear new app container
+        NRF_NVMC->CONFIG               = 0x00000002; // 2 == Erase enabled
+        NRF_NVMC->ERASEPAGE            = CONTAINER_APP_NEW;
+        while(NRF_NVMC->READY==0);
+        NRF_NVMC->CONFIG               = 0x00000000; // 0 == Read only access
+
+    }
+
+    //=== handover to app
+
+    // WARNING: no attempt to verify this code runs
+    // recommended fix:
+    //  - have a CRC in the image, check that before running
+    //  - arms a watchdog timer before handing over; cancel that watchdog from within the app if working
+    blue_vars.appfunc                  = (appfunc_t*)(CONTAINER_APP_CURRENT+1); // we shift by one byte to point to the right entry of the function
+    return blue_vars.appfunc();
 }
 
 //=========================== helpers =========================================
